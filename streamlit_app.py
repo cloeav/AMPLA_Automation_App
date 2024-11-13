eabb39c460cb4293bb9b0ad4bc78b5cba1524bdb
Cloe Artyounian Vieyra <76513475+cloeav@users.noreply.github.com>
2024-11-13 14:40:03 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import subprocess
 # Streamlit button to run the script
 if st.button('Run Another Script'):
     # Use subprocess to run the other script
-    result = subprocess.run(['python', r"C:\Users\cloea\Desktop\AMPLA_automation.py"], capture_output=True, text=True)
+    result = subprocess.run(['python', AMPLA_automation_git.py], capture_output=True, text=True)
     
     # Display the output of the script in the Streamlit app
     st.write(result.stdout)
