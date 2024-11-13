91a4ba52c63d7a622e66b56acf2dc72aa610a3c5
Cloe Artyounian Vieyra <76513475+cloeav@users.noreply.github.com>
2024-11-13 15:19:30 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,16 +7,9 @@ st.title("Run Python Script on Streamlit Cloud")
 
 # Button to trigger the execution of the script
 if st.button("Run Python Script"):
-    try:
-        # Construct the command to run the Python script with any arguments
-        command = 'AMPLA_automation_git.py'
+    # Construct the command to run the Python script with any arguments
+    command = 'AMPLA_automation_git.py'
         
-        # Run the Python script using subprocess
-        result = subprocess.run('python',command, shell=True, capture_output=True, text=True)
-        
-        # Display the output of the script
-        st.text(result.stdout)
-        st.text(result.stderr)
+    # Run the Python script using subprocess
+    result = subprocess.run('python',command, shell=True, capture_output=True, text=True)
     
-    except Exception as e:
-        st.error(f"Error: {e}")
